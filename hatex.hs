b77c91716ea0cd68e906436f080b36d89eedc7ae
Milo Buwalda <milo.buwalda@gmail.com>
2018-08-24 11:28:55 +0200

    Tree export fix. Expr tree & encodedExpr to latex both work

--- hatex.hs
+++ hatex.hs
@@ -1,5 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 
+import qualified Expressions as E
+
 import Text.LaTeX.Base
 import Text.LaTeX.Packages.Trees.Qtree
 import Text.LaTeX.Packages.Inputenc
@@ -10,20 +12,59 @@ import Text.LaTeX.Packages.Inputenc
 -- treeExample2 :: Tree Int
 -- treeExample2 = Node (Just 0) [Node Nothing [Leaf 1,Leaf 2] , Leaf 3]
 
-treeExample3 :: Tree LaTeX
-treeExample3 = Node (Just $ textbf $ textit "Bold and italic")
-                  [ Leaf $ textbf "Bold"
-                  , Leaf $ textit "Italic"
-                    ]
+treeExtended :: Tree LaTeX
+treeExtended = Node 
+  (Just $ "repeat until (goal reached)")
+    [ Node (Just $ textit "if (PathAhead)")
+      [ Leaf "Forward", Leaf "TurnLeft"
+      ]
+    ]
+
+treeShortened :: Tree LaTeX
+treeShortened = Node 
+  (Just $ "repeat")
+    [ Node (Just $ textit "(PathAhead)")
+      [ Leaf "Forward", Leaf "TurnLeft"
+      ]
+    ]
+
 
 -- Main
 
 main :: IO ()
 main = renderFile "tree.tex" example
 
+mainX :: E.Expr -> IO ()
+mainX e = renderFile "tree.tex" (expressionTex e)
+
+mainS :: String -> IO ()
+mainS s = renderFile "tree.tex" (strExprTex s)
+
 example :: LaTeX
 example = {-document-} theBody
 
+expressionTex :: E.Expr -> LaTeX
+expressionTex e = tree id (convertE e)
+
+strExprTex :: String -> LaTeX
+strExprTex s = tree id (convertE (E.decodeExpr s))
+
+convertE :: E.Expr      -> Tree LaTeX --String
+convertE    (E.WHILE x)  = Node (Just $ "repeat") [convertE x]
+convertE    (E.IF c l r) = Node (Just $ fromString $ show c) [convertE l, convertE r]
+convertE    (E.SEQ xs)   = Node (Just $ "repeat") $map convertE xs
+convertE    E.Forward    = Leaf "Forward"
+convertE    E.TurnLeft   = Leaf "TurnLeft"
+convertE    E.TurnRight  = Leaf "TurnRight"
+convertE    E.Empty      = Leaf "Empty"
+
+theBody :: LaTeX
+theBody =
+    -- maketitle
+ -- <> tree fromString treeExample1
+ -- <> rendertree treeExample2
+ {-<>-} tree id treeExtended
+    <> tree id treeShortened
 
 {-thePreamble :: LaTeX
 thePreamble =
@@ -32,10 +73,3 @@ thePreamble =
  <> usepackage [utf8] inputenc
  <> title "Examples with trees"
  <> author "Daniel Díaz"-}
-
-theBody :: LaTeX
-theBody =
-    -- maketitle
- -- <> tree fromString treeExample1
- -- <> rendertree treeExample2
- {-<>-} tree id treeExample3
